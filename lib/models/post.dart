7e8f7ffe4c9aef6a5a73280e941ca7e1bcb4c247
Jiyeon Park <helmrping@gmail.com>
2024-01-18 20:43:43 +0900

    Make post model and some datas

---
+++ lib/models/post.dart
@@ -0,0 +1,23 @@
+class Post {
+  final String title;
+  final String item;
+  final int money;
+  final DateTime startDate;
+  final DateTime endDate;
+  final String location;
+  final bool borrow;
+  final String imageUrl;
+  final String description;
+
+  Post({
+    required this.title,
+    required this.item,
+    required this.money,
+    required this.startDate,
+    required this.endDate,
+    required this.location,
+    required this.borrow,
+    required this.imageUrl,
+    required this.description,
+  });
+}
---
+++ lib/models/post_list.dart
@@ -0,0 +1,8 @@
+import '../models/post.dart';
+
+class PostList {
+  List<Post> posts;
+  int count;
+
+  PostList({required this.posts, required this.count});
+}
