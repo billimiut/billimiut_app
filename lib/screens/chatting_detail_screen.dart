b1fff0d61c13a75a37c63a7ac454100e02ce88be
sunwoong00 <sunwoong89@g.skku.edu>
2024-02-28 14:25:42 +0900

    chatting

--- lib/screens/chatting_detail_screen.dart
+++ lib/screens/chatting_detail_screen.dart
@@ -123,6 +123,8 @@ class _ChattingDetailState extends State<ChattingDetail> {
         (post) => post['post_id'] == widget.postId,
         orElse: () => null);
 
+    print("post: $post");
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -149,7 +151,10 @@ class _ChattingDetailState extends State<ChattingDetail> {
             ChattingPostDetail(
               index: index,
               postId: widget.postId,
-              imageUrl: post!["image_url"][0] ?? "",
+              imageUrl:
+                  post!["image_url"] != null && post["image_url"].isNotEmpty
+                      ? post["image_url"][0]
+                      : "",
               location: loadLocation(post["name"]),
               title: post["title"] ?? "",
               money: post["money"],
@@ -157,8 +162,12 @@ class _ChattingDetailState extends State<ChattingDetail> {
               endDate: formatDate(post["end_date"]),
               borrow: post["borrow"],
               status: post["status"] == "게시" ? "빌려주기" : post["status"],
+              neighborId: widget.neighborId,
               neighborNickname: widget.neighborNickname,
               item: post["item"] ?? "",
+              isButtonShowed: (post["borrow"] == false &&
+                      post["writer_id"] == user.userId) ||
+                  (post["borrow"] == true && post["writer_id"] != user.userId),
             ),
             const SizedBox(
               height: 20,
