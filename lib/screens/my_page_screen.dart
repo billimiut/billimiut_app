a6f99ae8a434b65f1bc1b06e57e580ff41aa3ec7
JIGOOOD <helmrping@gmail.com>
2024-09-11 16:52:27 +0900

    fix to show borrow/lend list in mypage

--- lib/screens/my_page_screen.dart
+++ lib/screens/my_page_screen.dart
@@ -5,7 +5,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:billimiut_app/screens/my_posts_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
@@ -34,6 +36,48 @@ class _MyPageState extends State<MyPage> {
       return DateFormat('MM/dd HH:mm').format(date);
     } else {
       return '날짜정보 없음';
+    }
+  }
+
+  // API에서 유저의 포스트를 가져오는 함수
+  Future<List<dynamic>> fetchPostsByUser(String userId, bool borrow, String status) async {
+    try {
+
+      var apiEndPoint = dotenv.get("API_END_POINT");
+      final response = await http.get(
+        Uri.parse('$apiEndPoint/post/personal/$userId?borrow=$borrow&status=$status'),
+      );
+      final body = utf8.decode(response.bodyBytes); // UTF-8로 디코딩
+
+      if (response.statusCode == 200) {
+        return jsonDecode(body);
+      } else {
+        throw Exception('포스트 로드 실패');
+      }
+    } catch (e) {
+      print("오류: $e");
+      return [];
+    }
+  }
+
+  // 두 번의 API 호출을 통해 데이터를 각각 가져옴
+  Future<List<dynamic>> fetchAllPostsByUser(String userId, bool borrow) async {
+    try {
+      List<dynamic> posts = [];
+
+      // "빌림중" 상태 데이터를 가져옴
+      List<dynamic> postsBorrowing = await fetchPostsByUser(userId, borrow, '빌림중');
+      // "종료" 상태 데이터를 가져옴
+      List<dynamic> postsEnded = await fetchPostsByUser(userId, borrow, '종료');
+
+      // 두 리스트를 결합
+      posts.addAll(postsBorrowing);
+      posts.addAll(postsEnded);
+
+      return posts;
+    } catch (e) {
+      print("오류: $e");
+      return [];
     }
   }
 
@@ -100,33 +144,48 @@ class _MyPageState extends State<MyPage> {
                       ],
                     ),
                   ),
-                  Column(
-                    children: user.borrowList.asMap().entries.map((entry) {
-                      int index = entry.key;
-                      var item = entry.value;
+                  FutureBuilder<List<dynamic>>(
+                    future: fetchAllPostsByUser(user.uuid, true),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                            child: CircularProgressIndicator()); // 로딩 스피너 표시
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('오류: ${snapshot.error}'));
+                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return Center(child: Text('게시물이 없습니다.'));
+                      } else {
+                        var postList = snapshot.data!;
 
-                      //print("빌린 item: $item");
-                      //print("image_url[0]: ${item["image_url"][0]}");
-                      return item != null
-                          ? TransactionItem(
-                              postId: item["post_id"],
-                              imageUrl: (item["image_url"] != null &&
-                                      item["image_url"].isNotEmpty)
-                                  ? item["image_url"][0]
-                                  : "",
-                              location:
-                                  item["detail_address"], // 카카오맵으로 수정하면 변경
-                              // (item["name"] == "")
-                              //     ? "위치 정보 없음"
-                              //     : item["name"],
-                              title: item["title"],
-                              price: item["price"],
-                              startDate: formatDate(item["start_date"]),
-                              endDate: formatDate(item["end_date"]),
-                              status: item["status"],
-                            )
-                          : Container();
-                    }).toList(),
+                        return Column(
+                          children: postList.asMap().entries.map((entry) {
+                            int index = entry.key;
+                            var item = entry.value;
+
+                            return item != null
+                                ? TransactionItem(
+                                    postId: item["post_id"],
+                                    imageUrl: (item["image_url"] != null &&
+                                            item["image_url"].isNotEmpty)
+                                        ? item["image_url"][0]
+                                        : "",
+                                    location: item["detail_address"] ??
+                                        "위치 정보 없음", // 위치 정보
+                                    title: item["title"] ?? "제목 없음", // 제목
+                                    price: item["price"] ?? "가격 정보 없음", // 가격
+                                    startDate: DateFormat('yy-MM-dd HH:mm')
+                                        .format(
+                                            DateTime.parse(item["start_date"])),
+                                    endDate: DateFormat('yy-MM-dd HH:mm')
+                                        .format(
+                                            DateTime.parse(item["end_date"])),
+                                    status: item["status"] ?? "상태 정보 없음", // 상태
+                                  )
+                                : Container();
+                          }).toList(),
+                        );
+                      }
+                    },
                   ),
                 ],
               ),
@@ -150,34 +209,48 @@ class _MyPageState extends State<MyPage> {
                       ],
                     ),
                   ),
-                  Column(
-                    children: user.lendList.asMap().entries.map((entry) {
-                      int index = entry.key;
-                      var item = entry.value;
-                      //print("빌려준 item: $item");
-                      //print("image_url[0]: ${item["image_url"][0]}");
-                      return item != null
-                          ? TransactionItem(
-                              postId: item["post_id"],
-                              imageUrl: (item["image_url"] != null &&
-                                      item["image_url"].isNotEmpty)
-                                  ? item["image_url"][0]
-                                  : "",
-                              location:
-                                  item["detail_address"], // 카카오맵으로 수정하면 변경
-                              // (item["name"] == "")
-                              //     ? "위치 정보 없음"
-                              //     : item["name"],
-                              title: item["title"],
-                              price: item["price"],
-                              startDate: DateFormat('yy-MM-dd HH:mm')
-                                  .format(DateTime.parse(item["start_date"])),
-                              endDate: DateFormat('yy-MM-dd HH:mm')
-                                  .format(DateTime.parse(item["end_date"])),
-                              status: item["status"],
-                            )
-                          : Container();
-                    }).toList(),
+                  FutureBuilder<List<dynamic>>(
+                    future: fetchAllPostsByUser(user.uuid, false),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                            child: CircularProgressIndicator()); // 로딩 스피너 표시
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('오류: ${snapshot.error}'));
+                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return Center(child: Text('게시물이 없습니다.'));
+                      } else {
+                        var postList = snapshot.data!;
+
+                        return Column(
+                          children: postList.asMap().entries.map((entry) {
+                            int index = entry.key;
+                            var item = entry.value;
+
+                            return item != null
+                                ? TransactionItem(
+                                    postId: item["post_id"],
+                                    imageUrl: (item["image_url"] != null &&
+                                            item["image_url"].isNotEmpty)
+                                        ? item["image_url"][0]
+                                        : "",
+                                    location: item["detail_address"] ??
+                                        "위치 정보 없음", // 위치 정보
+                                    title: item["title"] ?? "제목 없음", // 제목
+                                    price: item["price"] ?? "가격 정보 없음", // 가격
+                                    startDate: DateFormat('yy-MM-dd HH:mm')
+                                        .format(
+                                            DateTime.parse(item["start_date"])),
+                                    endDate: DateFormat('yy-MM-dd HH:mm')
+                                        .format(
+                                            DateTime.parse(item["end_date"])),
+                                    status: item["status"] ?? "상태 정보 없음", // 상태
+                                  )
+                                : Container();
+                          }).toList(),
+                        );
+                      }
+                    },
                   ),
                 ],
               ),
